b9e761d5d3d9e28bed6331d767628ece070ff193
Natalia Muzyka <nmuzyka2004@mail.ru>
2022-12-08 22:22:46 +0100

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1n
+*/
+Console.WriteLine("Введите трехзначное число");
+int number = int.Parse(Console.ReadLine()??"");
+int DigitTwoOfNumber = number / 10 - (number / 100) * 10;
+Console.WriteLine($"Второй цифрой числа {number}"+
+    $" является {DigitTwoOfNumber}");   
